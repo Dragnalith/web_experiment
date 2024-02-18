284daecad267ff13a9379e6f99b8e4f93c2b0b76
Marc Delorme <delormemarco@gmail.com>
2024-02-19 02:09:23 +0900

    In 'Board & Pawns' separate the board state from the Board component

--- board_and_pawns/src/Components/Board.tsx
+++ board_and_pawns/src/Components/Board.tsx
@@ -1,32 +1,30 @@
-import { useState } from 'react';
-import { BoardState, updateBoard} from '../Models/BoardState'
+import { BoardState } from '../Models/BoardState'
 
-export function Board() {
-  const [ boardState, setboardState] = useState(new BoardState());
-  
-  var textStateArray = boardState.cells.map(() => 'X')
-  for (const pawn of boardState.pawns) {
-      textStateArray[pawn.pos] = 'O';
-  }
+interface BoardCallback {
+  (index: number): void
+}
+
+interface BoardProps {
+  board: BoardState;
+  callback: BoardCallback
+  optionalProp?: string; // Optional prop with default type
+}
+export function Board(props: BoardProps) {
   return <>
-    <div>
-      <p>Current State: {textStateArray.join("")}</p>
-    </div>
     <div className="Board">
-      { boardState.cells.map((cell, index) => {
+      { props.board.cells.map((cell, index) => {
       const pos = 50 * index;
-      return <button
-          className="Cell" key={index} style={{top: '0px', left: pos +'px'}} 
-          onClick={() => setboardState(updateBoard(boardState, index))}>
+      return (<button className="Cell" key={index} style={{top: '0px', left: pos +'px'}} 
+          onClick={() => props.callback(index)}>
           <p>{cell}</p>
-          </button>
+          </button>);
       })}
-      { boardState.pawns.map((pawn, _) => {
-      const pos = 50 * pawn.pos;
-      const color = boardState.selected == pawn.pos ? 'red' : 'black';
-      return <div className="Pawn" key={pawn.id} style={{backgroundColor: color, top: '50px', left: pos +'px'}}>
+      { props.board.pawns.map((pawn, _) => {
+        const pos = 50 * pawn.pos;
+        const color = props.board.selected == pawn.pos ? 'red' : 'black';
+        return (<div className="Pawn" key={pawn.id} style={{backgroundColor: color, top: '50px', left: pos +'px'}}>
           <p>{pawn.id}</p>
-          </div>
+          </div>);
       })}
     </div>
   </>
--- board_and_pawns/src/index.tsx
+++ board_and_pawns/src/index.tsx
@@ -1,8 +1,16 @@
 import { createRoot } from 'react-dom/client';
+import { useState } from 'react';
 import { Board } from './Components/Board';
+import { BoardState, updateBoard } from './Models/BoardState'
 import './styles/style.css';
 
 function App() {
+  const [ boardState, setboardState] = useState(new BoardState());
+
+  var textStateArray = boardState.cells.map(() => 'X')
+  for (const pawn of boardState.pawns) {
+      textStateArray[pawn.pos] = 'O';
+  }
   return (
     <div>
       <h1>Board & Pawns</h1>
@@ -12,7 +20,10 @@ function App() {
         <li>Move: Click on a button with no pawn to move the selected pawn.</li>
         <li>Remove: Click on a button where a pawn is selected to remove it.</li>
       </ul>
-      <Board/>
+      <div>
+        <p>Instant State: {textStateArray.join("")}</p>
+      </div>
+      <Board board={boardState} callback={(index) => setboardState(updateBoard(boardState, index))}/>
     </div>
   );
 }
